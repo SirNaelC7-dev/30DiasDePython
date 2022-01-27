a314d5b5ac5cbf037ad74903e07e3b1dedaa6b5e
SirNaelC7-dev <naelsonfernandesdev@gmail.com>
2022-01-26 23:36:43 -0300

    30Dias de Python

---
+++ 1°_Dia_Introdução/introducao.py
@@ -0,0 +1,35 @@
+print(3+4)      #adição
+print(3-4)      #subtração
+print(3*4)      #multiplicação
+print(3/4)      #divisão
+
+print(3%4)      #resto da divisão
+print(3**4)     #potenciação
+print(3//4)     #divisão inteira
+
+nome = 'Naelson'                                                #variável string contendo meu nome
+nomeFamilia = 'Fernandes'                                       #variável string contendo meu sobrenome
+pais = 'Brasil'                                                 #variável contendo string com nome do meu país
+msg = 'Estou participando do desafio 30 dias de Python!'        #variável string contendo mensagem
+
+"""Variáveis contendo valores para fazer checagem dos tipos de dados"""
+
+var0 = 10
+var1 = 9.8
+var2 = 3.14
+var3 = (4-4j)
+var4 = ['Sir', 'Nael', 'C7']
+var5 = 'Naelson'
+var6 = 'Fernandes'
+var7 = 'Brasil'
+
+"""Exibição dos tipos de valores armazenados em cada variável"""
+
+print(type(var0))
+print(type(var1))
+print(type(var2))
+print(type(var3))
+print(type(var4))
+print(type(var5))
+print(type(var6))
+print(type(var7))
