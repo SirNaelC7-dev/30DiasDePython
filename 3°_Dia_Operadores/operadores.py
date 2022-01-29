4571977c60fa59bfff407adeba7c9830eca1488d
SirNaelC7-dev <naelsonfernandesdev@gmail.com>
2022-01-28 23:20:50 -0300

    3° Dia

---
+++ 3°_Dia_Operadores/operadores.py
@@ -0,0 +1,78 @@
+#Dia 3 - 30 Dias de Programação Python
+
+#Exercícios
+
+"""idade = int(20)
+altura = float(1.80)
+numero_complexo = 1+1j
+
+base = int(input('Insira um valor para a base:'))
+altura = int(input('Insira um valor para a altura:'))
+area = int(0.5 * (base * altura))
+
+lado_a = int(input('Insira o valor do lado A:'))
+lado_b = int(input('Insira o valor do lado B:'))
+lado_c = int(input('Insira o valor do lado C:'))
+perimetro_t = lado_a + lado_b + lado_c
+
+largura = int(input('Insira o valor da largura:'))
+comprimento = int(input('Insira o valor do comprimento:'))
+area = largura * comprimento
+perimetro_r = 2 * area
+
+raio_circulo = int(input('Insira o valor do raio do círculo:'))
+pi = 3.14
+area_c = pi * raio_circulo ** 2
+circunferencia = 2 * pi * raio_circulo
+
+
+
+x = (2 + y) / 2
+y = 2 * x - 2
+
+x = 6
+y = 10
+
+inclinacao = y * 2- y * 1/ x * 2 - x * 1
+
+x = -2
+y = x ^ 2 + 6 * x + 9
+
+print(len('python') != len('dragon'))
+
+print('on' in 'python' and 'on' in 'dragon')
+
+print('jargon' in 'I hope this course is not full of jargon')
+
+print('on' not in 'python' and 'on' not in 'dragon')
+
+var = len('python')
+var = float(var)
+var = str(var)
+
+num = int(input('Digite um número para checar se é par:'))
+print(num%2 == 0)
+
+print(7 // 3 == int(2.7))
+
+print('10' == 10)
+
+print(int(9.8) == 10)
+
+horas_trabalhadas = float(input('Digite a quantidade de horas trabalhadas: '))
+valor_hora = float(input('Digite a quantia que recebe por hora trabalhada: '))
+pagamento = horas_trabalhadas * valor_hora
+
+anos_de_vida = int(input('Quantos anos você se dá de expectativa de vida?'))
+segundos = anos_de_vida * 31536000
+print(segundos)"""
+
+list= ['\n',
+    1, 1, 1, 1, 1,'\n',
+    2, 1, 2, 4, 8,'\n',
+    3, 1, 3, 9, 27,'\n',
+    4, 1, 4, 16, 64,'\n',
+    5, 1, 5, 25, 125,'\n',
+]
+
+print(list)
