b8a5167ad7b9663a87a4c08c1dddd7ff3bb0c21a
SirNaelC7-dev <naelsonfernandesdev@gmail.com>
2022-02-01 17:14:46 -0300

    7° Dia

---
+++ 7°_Dia_Conjuntos/conjuntos.py
@@ -0,0 +1,39 @@
+"""it_companies = {'Facebook', 'Google', 'Microsoft', 'Apple', 'IBM', 'Oracle', 'Amazon'}
+
+print(len(it_companies))
+
+it_companies.add('Twitter')
+
+it_companies.update(['Ubisoft', 'Nintendo', 'Playstation'])
+
+it_companies.remove('Twitter')
+
+A = {19, 22, 24, 20, 25, 26}
+B = {19, 22, 20, 25, 26, 24, 28, 27}
+
+A.union(B)
+
+A.intersection(B)
+
+print(A.issubset(B))
+
+print(A.isdisjoint(B))
+
+AB = A.union(B)
+BA = B.union(A)
+
+print(A.symmetric_difference(B))
+
+del A, B
+
+age = [22, 19, 24, 25, 26, 24, 25, 24]
+
+age_set = set(age)
+
+print('tamanho da lista =',len(age),'\ntamanho do conjunto =' ,len(age_set))
+"""
+frase = 'I am a teacher and I love to inspire and teach people'
+frase = frase.split()
+frase = set(frase)
+frase.intersection(frase)
+print(frase)
