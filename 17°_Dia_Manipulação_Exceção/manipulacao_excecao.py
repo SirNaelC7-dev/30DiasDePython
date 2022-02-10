9f81d0456a1a186e4a3ebd23494b6d2784515f2e
SirNaelC7-dev <naelsonfernandesdev@gmail.com>
2022-02-09 23:13:27 -0300

    17º Dia

--- 17°_Dia_Manipulação_Exceção/manipulacao_excecao.py
+++ 17°_Dia_Manipulação_Exceção/manipulacao_excecao.py
@@ -1,3 +1,21 @@
+try:
+    name = input('Digite seu nome:')
+    year_born = input('Ano que nasceu:')
+    age = 2022 - int(year_born)
+    print(f'Eu sou {name}. Minha idade é {age}.')
+except TypeError:
+    print('TypeError')
+except ValueError:
+    print('ValueError')
+except ZeroDivisionError:
+    print('ZeroDivisionError')
+else:
+    print('Eu costumo correr com o bloco try.')
+finally:
+    print('Eu fui executado.')
+
+
+
 paises = ['Finland', 'Sweden', 'Norway','Denmark','Iceland', 'Estonia','Russia']
 *paises_nordicos, es, ru = paises
 
